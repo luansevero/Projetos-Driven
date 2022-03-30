36cb83c7259b7b155ac3b230d4c95774ce3808ee
LuanSevero <luansevero@icloud.com>
2022-03-30 17:36:23 -0300

    Icone de Verificação(Html + Java) + Barra de rolagem(css)

--- Documents/Driven/Projetos/projeto3-driveneats/script.js
+++ Documents/Driven/Projetos/projeto3-driveneats/script.js
@@ -1,39 +1,48 @@
+//Icone
+
 //Armazenamento do Pedido
-let prato = ""
-let valorprato = 0
-let bebida = ""
-let valorbebida = 0
-let sobremesa = ""
-let valorsobremesa = 0
+let prato = "";
+let valorprato = 0;
+let bebida = "";
+let valorbebida = 0;
+let sobremesa = "";
+let valorsobremesa = 0;
 //Alerta para pedido fora de ordem!
-let alertaprato = ""
-let alertabebida = ""
+let alertaprato = "";
+let alertabebida = "";
 //Sistema para desselecionar e para mudança do texto do botão!
-let systemprato = 0
-let systembebida = 0
-let systemsobremesa = 0
+let systemprato = 0;
+let systembebida = 0;
+let systemsobremesa = 0;
 //Arrays com Nome e Valores dos Pratos!
-let arraypratos = ["", "", ""]
-let arrayvalorespratos = ["", "", ""]
+let arraypratos = ["", "", ""];
+let arrayvalorespratos = ["", "", ""];
 var quantidadepratos = document.querySelectorAll("div.prato");
 
 function seletorPrato(nprato){
     if(systemprato == nprato){
         document.getElementById(`prato-${nprato}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+        document.getElementById(`selecionado-prato-${nprato}`).style.display = "none"
         systemprato = 0
         prato = ""
         valorprato = 0
         document.getElementById(`bebida-${systembebida}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+        document.getElementById(`selecionado-bebida-${systembebida}`).style.display = "none"
+        systembebida = 0
         document.getElementById(`sobremesa-${systemsobremesa}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+        document.getElementById(`selecionado-sobremesa-${systemsobremesa}`).style.display = "none"
+        systemsobremesa = 0
     } else {
         systemprato = nprato
         valorprato = arrayvalorespratos[nprato - 1]
         prato = arraypratos[nprato - 1]
         for(let i = 1; i <= quantidadepratos.length; i++){
             if( i != nprato){
-                    document.getElementById(`prato-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040"; 
+                    document.getElementById(`prato-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+                    document.getElementById(`selecionado-prato-${i}`).style.display = "none"  
             } else {
-                    document.getElementById(`prato-${nprato}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";    
+                    document.getElementById(`prato-${nprato}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+                    document.getElementById(`selecionado-prato-${i}`).style.display = "flex"    
             }
         }
     }
@@ -47,10 +56,13 @@ function seletorBebida(nbebida){
         alert(`Selecione um Prato antes`)
     } else if(systembebida == nbebida){
         document.getElementById(`bebida-${nbebida}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+        document.getElementById(`selecionado-bebida-${nbebida}`).style.display = "none"
         systembebida = 0
         bebida = ""
         valorbebida = 0
         document.getElementById(`sobremesa-${systemsobremesa}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+        document.getElementById(`selecionado-sobremesa-${systemsobremesa}`).style.display = "none"
+        systemsobremesa = 0
     } else {
         systembebida = nbebida
         valorbebida = arrayvaloresbebidas[nbebida - 1]
@@ -58,8 +70,10 @@ function seletorBebida(nbebida){
         for(let i = 1; i <= quantidadebebidas.length; i++){
             if( i != nbebida){
                     document.getElementById(`bebida-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040"; 
+                    document.getElementById(`selecionado-bebida-${i}`).style.display = "none"
             } else {
-                    document.getElementById(`bebida-${nbebida}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";    
+                    document.getElementById(`bebida-${nbebida}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+                    document.getElementById(`selecionado-bebida-${i}`).style.display = "flex"   
             }
         }
     }
@@ -77,18 +91,22 @@ function seletorSobremesa(nsobremesa){
     } else {
         if(systemsobremesa == nsobremesa){
             document.getElementById(`sobremesa-${nsobremesa}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+            document.getElementById(`selecionado-sobremesa-${nsobremesa}`).style.display = "none"
             systemsobremesa = 0
             sobremesa = ""
             valorsobremesa = 0
+            
         } else {
             systemsobremesa = nsobremesa
             valorsobremesa = arrayvaloressobremesas[nsobremesa - 1]
             sobremesa = arraysobremesas[nsobremesa - 1]
             for(let i = 1; i <= quantidadesobremesas.length; i++){
                 if( i != nsobremesa){
-                        document.getElementById(`sobremesa-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040"; 
+                        document.getElementById(`sobremesa-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+                        document.getElementById(`selecionado-sobremesa-${i}`).style.display = "none"
                 } else {
-                        document.getElementById(`sobremesa-${nsobremesa}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";    
+                        document.getElementById(`sobremesa-${nsobremesa}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+                        document.getElementById(`selecionado-sobremesa-${i}`).style.display = "flex"    
                 }
             }
         }
