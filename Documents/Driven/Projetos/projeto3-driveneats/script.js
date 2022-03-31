3242641486b19d8d079d9a838f62c85500f97e43
LuanSevero <luansevero@icloud.com>
2022-03-31 17:15:46 -0300

    Remoção de elementos(extras) - Botão de Pedido(Feito)

--- Documents/Driven/Projetos/projeto3-driveneats/script.js
+++ Documents/Driven/Projetos/projeto3-driveneats/script.js
@@ -14,119 +14,114 @@ let alertabebida = "";
 let systemprato = 0;
 let systembebida = 0;
 let systemsobremesa = 0;
-//Arrays com Nome e Valores dos Pratos!
-let arraypratos = ["", "", ""];
-let arrayvalorespratos = ["", "", ""];
-var quantidadepratos = document.querySelectorAll("div.prato");
+//Botao ficar Verde
+let confirmprato = 0
+let confirmbebida = 0
+let confirmsobremesa = 0
 //Finalizar Pedido
-let botaoverde = document.getElementById('botão')
-let botaotexto = document.getElementById('botão-texto')
+let botaoverde = document.getElementById('botão');
+let botaotexto = document.getElementById('botão-texto');
+let somadosvalores = 0
 
+
+// Seletor do Prato
+let arraypratos = ["Frango Yin Yang", "Executivo - Carne", "Executivo - Frango", "Strogonoff", "Feijoada"];
+let arrayvalorespratos = ["21.90", "18.90", "16.90", "22.90", "14.90"];
+var quantidadepratos = document.querySelectorAll("div.prato");
 function seletorPrato(nprato){
-    if(systemprato == nprato){
-        botaotexto.innerHTML = `Selecione os 3 itens para fechar o pedido`
-        document.getElementById(`prato-${nprato}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-        document.getElementById(`selecionado-prato-${nprato}`).style.display = "none"
-        systemprato = 0
-        prato = ""
-        valorprato = 0
-        document.getElementById(`bebida-${systembebida}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-        document.getElementById(`selecionado-bebida-${systembebida}`).style.display = "none"
-        systembebida = 0
-        document.getElementById(`sobremesa-${systemsobremesa}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-        document.getElementById(`selecionado-sobremesa-${systemsobremesa}`).style.display = "none"
-        systemsobremesa = 0
-        botaoverde.style.backgroundColor = "#CBCBCB";
-    } else {
-        systemprato = nprato
-        valorprato = arrayvalorespratos[nprato - 1]
-        prato = arraypratos[nprato - 1]
-        for(let i = 1; i <= quantidadepratos.length; i++){
+    systemprato = nprato
+    valorprato = Number(arrayvalorespratos[nprato - 1])
+    prato = arraypratos[nprato - 1]
+    for(let i = 1; i <= quantidadepratos.length; i++){
             if( i != nprato){
-                    document.getElementById(`prato-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-                    document.getElementById(`selecionado-prato-${i}`).style.display = "none"  
-            } else {
-                    document.getElementById(`prato-${nprato}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
-                    document.getElementById(`selecionado-prato-${i}`).style.display = "flex"  
-            }
+                document.getElementById(`prato-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+                document.getElementById(`selecionado-prato-${i}`).style.display = "none"  
+        } else {
+                document.getElementById(`prato-${nprato}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+                document.getElementById(`selecionado-prato-${i}`).style.display = "flex" 
+                confirmprato = 1
         }
-        botaotexto.innerHTML = `Selecione mais 2 itens para fechar o pedido`
+    }
+    if(confirmprato == 1 && confirmbebida == 1 && confirmsobremesa == 1){
+        botaoverde.style.backgroundColor = "green";
+        botaotexto.innerHTML = `Finalizar pedido`   
     }
 }
-
-let arraybebidas = ["", "", ""]
-let arrayvaloresbebidas = ["", "", ""]
+//Seletor das Bebidas
+let arraybebidas = ["Coca-Cola", "Água", "Matte-Leâo", "H20H!", "Dolly Guaraná"]
+let arrayvaloresbebidas = ["5.90", "2.90", "4.90", "5.90", "8.90"]
 var quantidadebebidas = document.querySelectorAll("div.bebida");
 function seletorBebida(nbebida){
-    if(systemprato <= 0){
-        alert(`Selecione um Prato antes`)
-    } else if(systembebida == nbebida){
-        botaotexto.innerHTML = `Selecione mais 2 itens para fechar o pedido` 
-        document.getElementById(`bebida-${nbebida}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-        document.getElementById(`selecionado-bebida-${nbebida}`).style.display = "none"
-        systembebida = 0
-        bebida = ""
-        valorbebida = 0
-        document.getElementById(`sobremesa-${systemsobremesa}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-        document.getElementById(`selecionado-sobremesa-${systemsobremesa}`).style.display = "none"
-        systemsobremesa = 0
-        botaoverde.style.backgroundColor = "#CBCBCB";
-    } else {
-        systembebida = nbebida
-        valorbebida = arrayvaloresbebidas[nbebida - 1]
-        bebida = arraybebidas[nbebida - 1]
-        for(let i = 1; i <= quantidadebebidas.length; i++){
-            if( i != nbebida){
-                    document.getElementById(`bebida-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040"; 
-                    document.getElementById(`selecionado-bebida-${i}`).style.display = "none"
-            } else {
-                    document.getElementById(`bebida-${nbebida}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
-                    document.getElementById(`selecionado-bebida-${i}`).style.display = "flex"   
-            }
+    systembebida = nbebida
+    valorbebida = Number(arrayvaloresbebidas[nbebida - 1])
+    bebida = arraybebidas[nbebida - 1]
+    for(let i = 1; i <= quantidadebebidas.length; i++){
+        if( i != nbebida){
+            document.getElementById(`bebida-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040"; 
+            document.getElementById(`selecionado-bebida-${i}`).style.display = "none"
+        } else {
+            document.getElementById(`bebida-${nbebida}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+            document.getElementById(`selecionado-bebida-${i}`).style.display = "flex"
+            confirmbebida = 1
         }
-        botaotexto.innerHTML = `Selecione mais 1 item para fechar o pedido` 
+    }
+    if(confirmprato == 1 && confirmbebida == 1 && confirmsobremesa == 1){
+        botaoverde.style.backgroundColor = "green";
+        botaotexto.innerHTML = `Finalizar pedido`   
     }
 }
-
-let arraysobremesas = ["", "", ""]
-let arrayvaloressobremesas = ["", "", ""]
+//Seletor das Sobremesas
+let arraysobremesas = ["Pudim", "Torta de Limão", "Mousse de Maracujá", "Brownie"]
+let arrayvaloressobremesas = ["7.90", "9.90", "11.90", "6.90"]
 var quantidadesobremesas = document.querySelectorAll("div.sobremesa");
 function seletorSobremesa(nsobremesa){
-    if(systemprato <= 0 && systembebida <= 0){
-        alert(`Selecione um Prato e uma Bebida antes`)
-    }
-    else if(systembebida <= 0){
-        alert(`Selecione uma Bebida antes`)
-    } else {
-        if(systemsobremesa == nsobremesa){
-            document.getElementById(`sobremesa-${nsobremesa}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-            document.getElementById(`selecionado-sobremesa-${nsobremesa}`).style.display = "none"
-            systemsobremesa = 0
-            sobremesa = ""
-            valorsobremesa = 0
-            botaoverde.style.backgroundColor = "#CBCBCB";
-            botaotexto.innerHTML = `Selecione mais 1 item para fechar o pedido` 
+    systemsobremesa = nsobremesa
+    valorsobremesa = Number(arrayvaloressobremesas[nsobremesa - 1])
+    sobremesa = arraysobremesas[nsobremesa - 1]
+    for(let i = 1; i <= quantidadesobremesas.length; i++){
+        if( i != nsobremesa){
+            document.getElementById(`sobremesa-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
+            document.getElementById(`selecionado-sobremesa-${i}`).style.display = "none"
         } else {
-            systemsobremesa = nsobremesa
-            valorsobremesa = arrayvaloressobremesas[nsobremesa - 1]
-            sobremesa = arraysobremesas[nsobremesa - 1]
-            for(let i = 1; i <= quantidadesobremesas.length; i++){
-                if( i != nsobremesa){
-                        document.getElementById(`sobremesa-${i}`).style.boxShadow = "inset 0px 0px 10px -4px #00000040";
-                        document.getElementById(`selecionado-sobremesa-${i}`).style.display = "none"
-                } else {
-                        document.getElementById(`sobremesa-${nsobremesa}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
-                        document.getElementById(`selecionado-sobremesa-${i}`).style.display = "flex"    
-                }
-            }
-            botaoverde.style.backgroundColor = "green";
-            botaotexto.innerHTML = `Finalizar Pedido` 
+            document.getElementById(`sobremesa-${nsobremesa}`).style.boxShadow = "0px 4px 4px rgba(0, 0, 0, 0.25), inset 0px 0px 0px 5px #32B72F";
+            document.getElementById(`selecionado-sobremesa-${i}`).style.display = "flex" 
+            confirmsobremesa = 1
         }
+    }
+    if(confirmprato == 1 && confirmbebida == 1 && confirmsobremesa == 1){
+        botaoverde.style.backgroundColor = "green";
+        botaotexto.innerHTML = `Finalizar pedido`   
     }
 }
 
+if(confirmprato == 1 && confirmbebida == 1 && confirmsobremesa == 1){
+    alert(`oi`)
+}
+/*Chields do Pedido*/
+let pratoPedido = document.getElementById("prato-pedido")
+let bebidaPedida = document.getElementById("bebida-pedida") 
+let sobremesaPedida = document.getElementById("sobremesa-pedida")
+let pratoPedidoValor = document.getElementById(`prato-pedido-valor`)
+let bebidaPedidaValor = document.getElementById(`bebida-pedida-valor`)
+let sobremePedidaValor = document.getElementById(`sobremesa-pedida-valor`)
+let subtotal = document.getElementById("subtotal")
 function finalizarPedido(){
     if(botaoverde.style.backgroundColor == "green"){
-        
+        document.getElementById(`pedidofinalizado`).style.display = 'flex'
+        pratoPedido.innerHTML = prato
+        pratoPedidoValor.innerHTML = valorprato.toFixed(2)
+        bebidaPedida.innerHTML = bebida
+        bebidaPedidaValor.innerHTML = valorbebida.toFixed(2)
+        sobremesaPedida.innerHTML = sobremesa
+        sobremePedidaValor.innerHTML = valorsobremesa.toFixed(2)
+        subtotal.innerHTML = `R$ ${(valorprato + valorsobremesa + valorbebida).toFixed(2)}`
     }
+}
+function pedidoFinalizado(){
+    let nome = prompt(`Qual o seu nome?`)
+    let endereco = prompt(`${nome}, qual o seu endereço?`)
+
+}
+function cancelar(){
+    document.getElementById(`pedidofinalizado`).style.display = 'none';
 }
