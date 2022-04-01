b1fa389fe36be62d2729ac9932df74622f199e24
LuanSevero <luansevero@icloud.com>
2022-04-01 16:08:14 -0300

    Projeto finalizado com bônus pedido

--- Documents/Driven/Projetos/projeto3-driveneats/script.js
+++ Documents/Driven/Projetos/projeto3-driveneats/script.js
@@ -1,21 +1,71 @@
-function seletorPrato(elemento){
-    const selecionado = document.querySelector(".selecionado")
-    if(selecionado !== null){
-        selecionado.classList.toggle("selecionado")
+//Nome dos Itens
+const confirmItem = document.querySelectorAll(`.main-left > .item-select-name`)
+//Preço dos Itens
+const confirmPrice = document.querySelectorAll(`.main-right > .item-select-price`)
+const total = document.querySelector(`.subtotal`)
+//Saber se está selecionado
+let pratoSelecionado;
+let bebidaSelecionada;
+let sobremesaSelecionada;
+//Dados do botão (Footer)
+const button = document.querySelector(".order-button");
+const buttonText = document.querySelector(".button-text");
+//Dados da Confirmação do Pedido
+const confirmscreen = document.querySelector(`.pedidofinalizado`)
+//Selecionar elementos
+function select(element){
+    const itemtype = element.classList[0]
+    const allitems = document.querySelectorAll(`.${itemtype}`)
+    const price = element.querySelector(".preço").innerHTML.replace("R$ ", "").replace(",", ".")
+    const itemName = element.querySelector(".nome-produto").innerHTML
+    for(let i = 0; i < allitems.length; i++){ 
+        if(allitems[i].classList.contains("selected")){
+            allitems[i].classList.remove("selected")
+            allitems[i].querySelector(`.icone-selecionado`).classList.remove("selected-icon")
+        }
     }
-    elemento.classList.add("selecionado")
+    element.querySelector(".icone-selecionado").classList.add("selected-icon")
+    element.classList.add("selected")
+    if(itemtype == "prato"){
+        confirmItem[0].innerHTML = itemName 
+        confirmPrice[0].innerHTML = Number(price).toFixed(2)
+        pratoSelecionado = true
+        orderDone()
+    } else if (itemtype == "bebida"){
+        confirmItem[1].innerHTML = itemName
+        confirmPrice[1].innerHTML = Number(price).toFixed(2)
+        bebidaSelecionada = true
+        orderDone()
+    } else {
+        confirmItem[2].innerHTML = itemName
+        confirmPrice[2].innerHTML = Number(price).toFixed(2)
+        sobremesaSelecionada = true
+        orderDone()
+    }
 }
-function seletorBebida(elemento){
-    const selecionado = document.querySelector(".selecionado")
-    if(selecionado !== null){
-        selecionado.classList.toggle("selecionado")
+//Função para mudar a cor do botão e texto!
+function orderDone(){
+    if(pratoSelecionado && bebidaSelecionada && sobremesaSelecionada){
+        button.style.backgroundColor = "green";
+        buttonText.innerHTML = "Finalizar Pedido"
+        total.innerHTML = `R$ ${(Number(confirmPrice[0].innerHTML) + Number(confirmPrice[1].innerHTML) + Number(confirmPrice[2].innerHTML)).toFixed(2)}`
     }
-    elemento.classList.add("selecionado")
 }
-function seletorSobremesa(elemento){
-    const selecionado = document.querySelector(".selecionado")
-    if(selecionado !== null){
-        selecionado.classList.toggle("selecionado")
+//Função para aparecer a tela de confirmação do pedido!(bônus do Projeto)
+function finalizarPedido(){
+    if(button.style.backgroundColor == "green"){
+        confirmscreen.style.display = "flex";
     }
-    elemento.classList.add("selecionado")
+}
+//Função para ir para o ZAPZAP e terminar o Pedido
+function pedidoFinalizado(){
+    const name = prompt(`Qual é o seu nome?`)
+    const adress = prompt(`Qual o endereço da entrega?`)
+    let msg = `Olá, gostaria de fazer o pedido:\n- Prato: ${confirmItem[0].innerHTML}\n- Bebida: ${confirmItem[1].innerHTML}\n- Sobremesa: ${confirmItem[2].innerHTML}\nTotal: ${total.innerHTML}\n\nNome: ${name}\nEndereço: ${adress}`
+    mensagem = window.encodeURIComponent(msg);
+    window.open(`https://wa.me/5521969592572?text=${msg}`);
+}
+//Função para cancelar a compra!(bônus do Projeto)
+function cancelar(){
+    confirmscreen.style.display = "none";
 }
