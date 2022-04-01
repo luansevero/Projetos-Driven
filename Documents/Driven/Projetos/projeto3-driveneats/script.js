a44900cba73674daccf98a56b85f70953c7f2449
LuanSevero <luansevero@icloud.com>
2022-03-31 22:53:46 -0300

    Função de selecionar item

--- Documents/Driven/Projetos/projeto3-driveneats/script.js
+++ Documents/Driven/Projetos/projeto3-driveneats/script.js
@@ -1,2 +1,21 @@
-
-
+function seletorPrato(elemento){
+    const selecionado = document.querySelector(".selecionado")
+    if(selecionado !== null){
+        selecionado.classList.toggle("selecionado")
+    }
+    elemento.classList.add("selecionado")
+}
+function seletorBebida(elemento){
+    const selecionado = document.querySelector(".selecionado")
+    if(selecionado !== null){
+        selecionado.classList.toggle("selecionado")
+    }
+    elemento.classList.add("selecionado")
+}
+function seletorSobremesa(elemento){
+    const selecionado = document.querySelector(".selecionado")
+    if(selecionado !== null){
+        selecionado.classList.toggle("selecionado")
+    }
+    elemento.classList.add("selecionado")
+}
